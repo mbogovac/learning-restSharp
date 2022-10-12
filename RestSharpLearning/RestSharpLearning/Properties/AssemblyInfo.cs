7f1925cda74dc3b0983b45dd70c4e27c0bc7ca64
milabogovac <bogovac.mila24@gmail.com>
2022-10-12 14:41:18 +0200

    restsharp project

---
+++ RestSharpLearning/RestSharpLearning/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpLearning")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Levi9")]
+[assembly: AssemblyProduct("RestSharpLearning")]
+[assembly: AssemblyCopyright("Copyright © Levi9 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8c60404-66ad-4331-9069-8fdb473cf366")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpLearning/RestSharpLearning/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Dynamitey.DynamicObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RestSharp.Serialization.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RestSharpLearning
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public string TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}", Method.Get);
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            //ovo ne radi
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            JObject obs = JObject.Parse(response.Content);
+            return obs[response].ToString();
+        }
+    }
+}
